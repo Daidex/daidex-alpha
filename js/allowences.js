af6ec7ded156552a50c70c5f53d36fc788ef6aaf
César Pineda <cesar.pineda@coinbtr.com>
2018-07-18 10:17:38 -0500

    functionalitied added

--- js/allowences.js
+++ js/allowences.js
@@ -7,19 +7,34 @@ if (typeof web3 !== 'undefined') {
 }
 
 async function modifyAllowence(symbol){
-  var checkedValue = document.querySelector('.messageCheckbox').checked;
+  var checkedValue = document.getElementById(symbol.toLowerCase() + '-checkbox').checked;
+  document.getElementById(symbol.toLowerCase() + '-checkbox').disabled = true;
+  document.getElementById(symbol.toLowerCase() + '-loader').hidden = false;
+  document.getElementById(symbol.toLowerCase() + '-span').hidden = true;
   let account = await get_user_account();
   let netId = await getNetwork();
   let tokenAddress = await getTokenAddressByNetId(symbol, netId);
+  const zeroEx = new ZeroEx.ZeroEx(web3.currentProvider, { networkId: netId });
+  let msg;// msg can be an error if transaction rejected or tx hash if success
   if (checkedValue == true){
-    let txId = await enableTokenTrading(tokenAddress, account, netId);
-    document.getElementById(symbol.toLowerCase() + '-msg').innerHTML = txId;
-    console.log(txId);
+    try{
+      msg = await enableTokenTrading(tokenAddress, account, netId);
+      await zeroEx.awaitTransactionMinedAsync(msg, 1500);
+    }
+    catch(error){
+      console.log(error);
+      msg = error;
+    }
   } else {
-    let txId = await disableTokenTrading(tokenAddress, account, netId);
-    document.getElementById(symbol.toLowerCase() + '-msg').innerHTML = txId;
-    console.log(txId);
+    try{
+      msg = await disableTokenTrading(tokenAddress, account, netId);
+      await zeroEx.awaitTransactionMinedAsync(msg, 1500);
+    }catch(error){
+      msg = error;
+    }
   }
+  document.getElementById(symbol.toLowerCase() + '-msg').innerHTML = msg;
+  loadTokenAllowences(symbol);
 }
 
 async function enableTokenTrading(tokenAddress, accountAddress, netId){
@@ -37,11 +52,13 @@ async function enableTokenTrading(tokenAddress, accountAddress, netId){
 }
 
 async function disableTokenTrading(tokenAddress, accountAddress, netId){
+  let amount = new BigNumber.BigNumber("0");
+  console.log(typeof amount)
   const zeroEx = new ZeroEx.ZeroEx(web3.currentProvider, { networkId: netId });
   const disableTx = await zeroEx.token.setProxyAllowanceAsync(
     tokenAddress,
     accountAddress,
-    new BigNumber.BigNumber("0"),
+    amount,
     {
       // You can optionally pass in the gas price and gas limit you would like to use
       // gasLimit: 80000,
@@ -111,19 +128,108 @@ async function getTokenAddressByNetId(symbol, networkId){
         default:
           return undefined;
       }
+      break;
+    case "DAI":
+      switch (networkId) {
+        case 1:
+          return networks.Mainnet.DAI;
+          break;
+        case 3:
+          return networks.Ropsten.DAI;
+          break;
+        case 4:
+          return networks.Rinkeby.DAI;
+          break;
+        case 42:
+          return networks.Kovan.DAI;
+          break;
+        default:
+          return undefined;
+      }
+      break;
+    case "DGD":
+      switch (networkId) {
+        case 1:
+          return networks.Mainnet.DGD;
+          break;
+        case 3:
+          return networks.Ropsten.DGD;
+          break;
+        case 4:
+          return networks.Rinkeby.DGD;
+          break;
+        case 42:
+          return networks.Kovan.DGD;
+          break;
+        default:
+          return undefined;
+      }
+      break;
+    case "MKR":
+      switch (networkId) {
+        case 1:
+          return networks.Mainnet.MKR;
+          break;
+        case 3:
+          return networks.Ropsten.MKR;
+          break;
+        case 4:
+          return networks.Rinkeby.MKR;
+          break;
+        case 42:
+          return networks.Kovan.MKR;
+          break;
+        default:
+          return undefined;
+      }
+      break;
+    case "REP":
+      switch (networkId) {
+        case 1:
+          return networks.Mainnet.REP;
+          break;
+        case 3:
+          return networks.Ropsten.REP;
+          break;
+        case 4:
+          return networks.Rinkeby.REP;
+          break;
+        case 42:
+          return networks.Kovan.REP;
+          break;
+        default:
+          return undefined;
+      }
+      break;
     default:
       return undefined;
   }
 }
 
-async function getTokenAllowence(){
+async function getTokenAllowence(symbol){
   let netId = await getNetwork();
   const zeroEx = new ZeroEx.ZeroEx(web3.currentProvider, { networkId: netId });
   let balance = await zeroEx.token.getProxyAllowanceAsync(
-    await getTokenAddressByNetId("WETH", netId),
+    await getTokenAddressByNetId(symbol, netId),
     await get_user_account(),
   );
   return balance.toNumber();
 }
 
-console.log(getTokenAllowence());
+async function loadTokenAllowences(symbol){
+  if (await getTokenAllowence(symbol) > 0){
+    document.getElementById(symbol.toLowerCase() + '-checkbox').checked = true;
+  }else{
+    document.getElementById(symbol.toLowerCase() + '-checkbox').checked = false;
+  }
+  document.getElementById(symbol.toLowerCase() + '-checkbox').disabled = false;
+  document.getElementById(symbol.toLowerCase() + '-loader').hidden = true;
+  document.getElementById(symbol.toLowerCase() + '-span').hidden = false;
+}
+
+loadTokenAllowences("WETH");
+loadTokenAllowences("ZRX");
+loadTokenAllowences("DAI");
+loadTokenAllowences("DGD");
+loadTokenAllowences("MKR");
+loadTokenAllowences("REP");
